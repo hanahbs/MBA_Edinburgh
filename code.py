15b6baff2d4f62c233cb77c11877c2c9efadbae3
hanahbs <hanahbs1@gmail.com>
2024-07-17 12:20:11 +0700

    tambah kode nya

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mlxtend.frequent_patterns import apriori, association_rules
+from mlxtend.preprocessing import TransactionEncoder
+from pyvis.network import Network
+import datetime as dt
+import numpy as np
